3f01e575f78c7e6f24e4862b3dd65b6157398665
KozaX <55134615+KozaX@users.noreply.github.com>
2020-06-30 20:52:58 +0200

    remove methods, use only strings and operators, complete all the bonus exercises

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ let hacker2 = 'Messing Withyou';
 let message;
 
 console.log("The driver's name is " + hacker1);
-console.log('The navigators name is ' + hacker2);
+console.log("The navigator's name is " + hacker2);
 
 // Iteration 2: Conditionals
 if (hacker1.length > hacker2.length) {
@@ -20,45 +20,107 @@ if (hacker1.length > hacker2.length) {
 
 // Iteration 3: Loops
 
-//3.1
-let driver = hacker1.split('');
-console.log(driver);
-driver = driver.join(' ').toUpperCase();
-console.log(driver);
+/*3.1 Print all the characters of the driver's name, separated by a space and
+ [in capitals](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase)
+  i.e. `"J O H N"`
+*/
+let auxString = '';
+for (let i = 0; i < hacker1.length; i++) {
+  if (i < hacker1.length - 1) {
+    auxString += hacker1[i] + ' ';
+  } else {
+    auxString += hacker1[i];
+  }
+}
 
-//3.2
-let drunkDriver = hacker1.split('');
-drunkDriver.reverse();
-drunkDriver = drunkDriver.join('');
-console.log(drunkDriver);
+console.log(auxString);
 
-//3.3
+/*3.2 Print all the characters of the navigator's name, in reverse order. 
+  i.e. `"nhoJ"`*/
+let auxReverseString = '';
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  auxReverseString += hacker2[i];
+}
+console.log(auxReverseString);
 
-if (hacker1 < hacker2) {
-  console.log("The driver's name goes first.");
-} else if (hacker1 > hacker2) {
-  console.log('Yo, the navigator goes first definitely.');
+/*3.3 Depending on the [lexicographic order](https://en.wikipedia.org/wiki/Lexicographical_order) of the strings, print: <br>
+      - `The driver's name goes first.` <br>
+      - `Yo, the navigator goes first definitely.` <br>
+      - `What?! You both have the same name?`*/
+if (hacker1.toUpperCase() < hacker2.toUpperCase()) {
+  console.log(`The driver's name goes first.`);
+} else if (hacker1.toUpperCase() > hacker2.toUpperCase()) {
+  console.log(`Yo, the navigator goes first definitely.`);
 } else {
   console.log(`What?! You both have the same name?`);
 }
 
-// Bonus 1
+/* Bonus 1:
+Go to [lorem ipsum generator](http://www.lipsum.com/) and:
+  - Generate 3 paragraphs. Store the text in a variable type of string.
+  - Make your program count the number of words in the string.
+  - Make your program count the number of times the Latin word [`et`](https://en.wiktionary.org/wiki/et#Latin) appears.*/
 
-let loremIpsum =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam id tempus dolor. Vivamus sagittis ligula dolor, vitae ullamcorper urna vestibulum in. Duis commodo tincidunt diam eu convallis. Donec tincidunt scelerisque dui eu luctus. Phasellus non felis nulla. Morbi in elementum justo, eget sagittis justo. Sed eu lacus venenatis, tristique erat sit amet, pulvinar est.Phasellus sit amet augue a magna mollis accumsan. Maecenas dui risus, molestie vitae semper rutrum, condimentum ultrices risus. Aliquam eget posuere eros, vitae faucibus quam. Fusce aliquet aliquet metus nec convallis. Integer venenatis purus sed sapien sodales, ut auctor diam laoreet. Vestibulum pellentesque sapien id elementum pellentesque. Phasellus nec aliquet neque. Donec lectus ex, vestibulum et ipsum sed, facilisis sollicitudin lectus. Fusce aliquet ultricies quam at sodales. Fusce mi neque, posuere non diam vulputate, feugiat fringilla purus. Suspendisse eu fermentum tellus, non commodo ante. Vestibulum condimentum nisi ut metus porta, in vehicula lacus condimentum. Etiam dapibus pretium metus, sed condimentum odio dapibus sit amet. Suspendisse risus mi, egestas sit amet iaculis eget, fermentum nec orci. Suspendisse potenti. Donec lacus risus, commodo sit amet consectetur ac, imperdiet sed mauris.Proin aliquet neque at eleifend sodales. Vivamus facilisis commodo pulvinar. Nunc ante nulla, accumsan id tortor in, iaculis mattis velit. Nullam vitae tellus gravida, ornare arcu id, finibus justo. Morbi nec tincidunt metus. Morbi sodales in sapien vitae ullamcorper. Aenean ut ultricies ligula. Donec in nunc sed dui volutpat accumsan vel vel metus. Etiam cursus enim ut ultricies hendrerit.';
-let allWords = loremIpsum.split(' ');
-let countWords = 0;
-//number of words
-for (let i = 0; i < allWords.length; i++) {
-  countWords++;
-}
-console.log(countWords);
-//number os et's
-let countEts = 0;
-for (let i = 0; i < allWords.length; i++) {
-  if (allWords[i] === 'et') {
-    countEts++;
+const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed convallis enim eget rhoncus rhoncus. Sed a ex ut urna interdum varius ac quis sem. Suspendisse bibendum tellus sit amet nibh tristique imperdiet. Sed aliquam lectus eu ultrices lobortis. Fusce ac eleifend erat, eget mattis massa. Aenean vehicula vulputate convallis. Phasellus diam tortor, aliquam vitae aliquet id, placerat venenatis nulla. Integer porta ex id auctor eleifend. Nulla facilisi. Donec eget quam nisl. Duis non eros metus. In hac habitasse platea dictumst.
+
+  Vivamus non vehicula orci. Integer nec ligula ante. Nullam quis neque vitae enim sollicitudin aliquet sed et quam. Suspendisse blandit vitae purus vitae hendrerit. Pellentesque vitae cursus turpis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus commodo diam ac nibh posuere viverra. Mauris accumsan, urna quis rhoncus rutrum, eros diam pulvinar nulla, et pellentesque massa odio non orci. Nunc nunc risus, lobortis ac lacinia in, mattis quis mi. Sed tincidunt tincidunt semper. Mauris eu quam dolor. Praesent nulla urna, vestibulum in mi vel, cursus viverra erat. Vivamus sed scelerisque odio. Ut eu leo porta, pellentesque lorem ut, ullamcorper dolor. Aliquam ac nibh ut ligula eleifend tincidunt.
+  
+  Sed vel molestie lacus. In faucibus aliquet turpis, eu feugiat odio aliquet a. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin sit amet ante eu eros mollis rutrum eu sit amet leo.et Sed at sapien eu nisl consequat pellentesque. Nullam ac aliquam purus. Aenean sed fermentum justo. Phasellus pretium augue sed sodales faucibus. Cras eget gravida lacus. Nullam bibendum mattis libero et quis etfaucibus.`;
+let wordCount = 0;
+for (let i = 0; i < lorem.length; i++) {
+  if (lorem[i] === ' ') {
+    wordCount++;
   }
 }
+if (lorem) {
+  wordCount += 1;
+}
+console.log('The lorem has ' + wordCount + ' words');
 
-console.log(countEts);
+let etCount = 0;
+for (let i = 0; i < lorem.length; i++) {
+  if (
+    lorem[i] === ' ' &&
+    lorem[i + 1].toUpperCase() === 'E' &&
+    lorem[i + 2].toUpperCase() === 'T' &&
+    (lorem[i + 3].toUpperCase() <= 'A' || lorem[i + 3].toUpperCase() >= 'Z')
+  ) {
+    etCount++;
+  }
+}
+console.log(`There are ${etCount} ets in the text.`);
+
+/*#### Bonus 2:
+Create a new variable `phraseToCheck` and have it contain some string value. Write a code that will check if the value we assigned to this variable is a [Palindrome](https://en.wikipedia.org/wiki/Palindrome). Here are some examples of palindromes: 
+  - "A man, a plan, a canal, Panama!"
+  - "Amor, Roma"
+  - "race car"
+  - "stack cats"
+  - "step on no pets"
+  - "taco cat"
+  - "put it up"
+  - "Was it a car or a cat I saw?" and "No 'x' in Nixon". */
+
+let parseToCheck = 'race car';
+
+let auxParseString = '';
+
+for (let i = 0; i < parseToCheck.length; i++) {
+  if (parseToCheck[i] >= 'A' && parseToCheck[i] <= 'z') {
+    auxParseString += parseToCheck[i];
+  }
+}
+let simpleParseString = auxParseString.toLowerCase();
+console.log(simpleParseString);
+
+let reverseString = '';
+for (let i = simpleParseString.length - 1; i >= 0; i--) {
+  reverseString += simpleParseString[i];
+}
+console.log(reverseString);
+
+if (simpleParseString === reverseString) {
+  console.log(`It's a Palindrome`);
+} else {
+  console.log(`It's not a Palindrome`);
+}
